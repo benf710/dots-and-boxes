877375a948a7321cbf7a961776ac7e6ad08279a6
Ben Foltz <ben.h.foltz@gmail.com>
2020-06-28 16:10:57 -0400

    client refactor

--- client.py
+++ client.py
@@ -2,39 +2,59 @@ import json
 import requests
 import time
 
+class Game():
+    def __init__(self, host, player_name, game_id=None):
+        self.server = host
+        self.player = player_name
+        if game_id:
+            self.game_id = game_id
+            self.status = self.join_game()
+        else:
+            self.game_id = self.create_game()
 
-def get_all_games(server):
-    result = json.loads(requests.get('http://' + server + '/api/all').text)
-    return result
+    def get_all_games(self):
+        result = json.loads(requests.get('http://' + self.server + '/api/all').text)
+        return result
 
+    def create_game(self):
+        result = json.loads(requests.get('http://' + self.server + '/api/new', params={'player_name': self.player}).text)
+        if result['status'] == 'failed':
+            raise ValueError(f'Bad response: {result}')
+        return result['room']
 
-def create_game(server, player):
-    result = json.loads(requests.get('http://' + server + '/api/new', params={'player_name': player}).text)
-    if result['status'] == 'failed':
-        raise ValueError(f'Bad response: {result}')
-    return result
+    def join_game(self):
+        result = json.loads(requests.get('http://' + self.server + f'/api/room/{self.game_id}/join/{self.player}').text)
+        if result['status'] == 'failed':
+            raise ValueError(f'Bad response: {result}')
+        return result
 
+    def get_game_status(self):
+        result = json.loads(requests.get('http://' + self.server + f'/api/room/{self.game_id}/status').text)
+        if result['status'] == 'failed':
+            raise ValueError(f'Bad response: {result}')
+        return result['result']['status']
 
-def join_game(server, room, player):
-    result = json.loads(requests.get('http://' + server + f'/api/room/{room}/join/{player}').text)
-    if result['status'] == 'failed':
-        raise ValueError(f'Bad response: {result}')
-    return result
+    def get_game_state(self):
+        result = json.loads(requests.get('http://' + self.server + f'/api/room/{self.game_id}/status').text)
+        if result['status'] == 'failed':
+            raise ValueError(f'Bad response: {result}')
+        return result['result']
 
-
-def get_game_status(server, room):
-    result = json.loads(requests.get('http://' + server + f'/api/room/{room}/status').text)
-    if result['status'] == 'failed':
-        raise ValueError(f'Bad response: {result}')
-    return result
-
-
-def move(server, room, player, move):
-    result = json.loads(
-        requests.post('http://' + server + f'/api/room/{room}/move', json={'player': player, 'move': move}).text)
-    if result['status'] == 'failed':
-        raise ValueError(f'Bad response: {result}')
-    return result
+    def move(self):
+        valid_input = False
+        while not valid_input:
+            try:
+                next_move = input('Enter a move (x, y, vector): ')
+                x, y, vector = next_move.split(',')
+                resp = requests.post('http://' + self.server + f'/api/room/{self.game_id}/move', 
+                json={'player': self.player, 'move': {'x': int(x), 'y': int(y), 'vector': vector}})
+                result = json.loads(resp.text)
+                if result['status'] == 'failed':
+                    raise ValueError(result['message'])
+                valid_input = True
+            except ValueError as e:
+                print(f'Invalid input! {e}')
+        return result
 
 
 def print_board(board):
@@ -60,31 +80,25 @@ if __name__ == '__main__':
     name = ''
     while name.strip() == '':
         name = input('Enter you player name: ')
-    game = input('Enter a game to join [Blank to create a new game]: ')
-    if game:
-        status = join_game(server, game, name)['result']['status']
-    else:
-        game = create_game(server, name)['room']
-        status = 'Waiting for another player to join...'
-    print(get_game_status(server, game))
-    if not status == 'In progress':
+    game_id = input('Enter a game to join [Blank to create a new game]: ')
+    game = Game(server, name, game_id=game_id)
+    if not game.get_game_status() == 'In progress':
+        print(f'Game {game.game_id} created.')
         print('Waiting for other player to join...')
-        while not status == 'In progress':
-            time.sleep(3)
-            status = get_game_status(server, game)['result']['status']
-    while get_game_status(server, game)['result']['status'] == 'In progress':
-        result = get_game_status(server, game)['result']
-        current_player_turn = result['turn']
-        game_status = result['status']
+        while not game.get_game_status() == 'In progress': time.sleep(3)
+    while game.get_game_status() == 'In progress':
+        state = game.get_game_state()
+        current_player_turn = state['turn']
+        game_status = state['status']
         if not current_player_turn == name:
             print('Waiting for other player to move...')
             while not current_player_turn == name and game_status == 'In progress':
                 time.sleep(3)
-                result = get_game_status(server, game)['result']
-                current_player_turn = result['turn']
-                game_status = result['status']
-        print_board(get_game_status(server, game)['result']['board'])
-        next_move = input('Enter a move [Blank to create a new game]: ')
-        x, y, vector = next_move.split(',')
-        print(move(server, game, name, {'x': int(x), 'y': int(y), 'vector': vector}))
-    print(get_game_status(server, game))
+                state = game.get_game_state()
+                current_player_turn = state['turn']
+                game_status = state['status']
+        print_board(game.get_game_state()['board'])   
+        if game_status == 'In progress':
+            result = game.move()
+        print(result)
+    print(game.get_game_status())
