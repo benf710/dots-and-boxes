feb970d63d80cb13170de05a9ead2b2fc0c8ec6b
Ben Foltz <ben.h.foltz@gmail.com>
2020-06-28 16:11:57 -0400

    bug fixes and formatting

--- static/game-ui.js
+++ static/game-ui.js
@@ -38,14 +38,15 @@ function togglePlayerDiag() {
   }
 
 function change_player(){
-    //player_name = prompt("Enter your player name");
+    document.getElementById("playername-error-message").style.display = "none"
     player_name = document.getElementById("playernameinput").value;
-    if (player_name != ""){
+    name_rex = /^[a-zA-Z0-9]+$/
+    if (name_rex.test(player_name)){
         setCookie("player_name", player_name);
         setPlayernameUI(player_name);
         togglePlayerDiag();
     } else {
-        console.log("Enter a playername");
+        document.getElementById("playername-error-message").style.display = "block"
     }
 }
 
@@ -56,31 +57,27 @@ function getAllGames(){
         if (this.readyState == 4 && this.status == 200) {
             games_obj = JSON.parse(request.response);
             game_ids = Object.keys(games_obj);
-            console.log("Request completed");
             if (game_ids.length == 0){
-                document.getElementById("list-games-view").innerHTML = '<p class="lead">No games available.</p>';
+                document.getElementById("games-list-message").innerHTML = '<p class="lead">No games available.</p>';
             } else {
+                document.getElementById("games-list-message").innerHTML = '';
                 list_builder = "";            
                 game_ids.forEach (function(element) {
-                    console.log(element);
                     valid_id_rex = /^[0-9]+$/;
                     if (valid_id_rex.test(element)){
-                        console.log('id passed');
-                        valid_value_rex = /^[a-zA-Z0-9\. ]+$/;
+                        valid_value_rex = /^[a-zA-Z0-9\.: ]+$/;
                         game_object = games_obj[element];
                         status = game_object['status'];
                         if (valid_value_rex.test(status)){
-                            console.log('status passed');
-                            list_builder += `<li><a href="/api/room/${element}/status">${element} - ${status}</a></li>`;
+                            list_builder += `<li><button class="btn btn-link" onclick="viewGame(${element});">${element} - ${status}</button>`
+                            //list_builder += `<li><a href="/api/room/${element}/status">${element} - ${status}</a></li>`;
                         } else {
-                            document.getElementById("list-games-view").innerHTML = '<p class="lead">Invalid response from server.</p>';
+                            document.getElementById("games-list-message").innerHTML = '<p class="lead text-danger">Invalid response from server.</p>';
                             error = true;
-                            console.log('error at valid_status');
                         }
                     } else {
-                        document.getElementById("list-games-view").innerHTML = '<p class="lead">Invalid response from server.</p>';
+                        document.getElementById("games-list-message").innerHTML = '<p class="lead text-danger">Invalid response from server.</p>';
                         error = true;
-                        console.log('error at valid_id');
                     }
                 })
                 if (error == false){
@@ -96,15 +93,35 @@ function getAllGames(){
 
 function toggleView(view) {
     pn_diag = document.getElementById("playername-diag");
-    if (pn_diag.style.display === "none") {
-        index_view = document.getElementById("index-view");
-        list_games_view = document.getElementById("list-games-view");
-        in_game_view = document.getElementById("in-game-view");
-        index_view.style.display = "none";
-        in_game_view.style.display = "none";
-        list_games_view.style.display = "none";
-        document.getElementById(view).style.display = "block";
+    if (pn_diag.style.display === "block") {
+        togglePlayerDiag();
     }
+    index_view = document.getElementById("index-view");
+    list_games_view = document.getElementById("list-games-view");
+    in_game_view = document.getElementById("in-game-view");
+    index_view.style.display = "none";
+    in_game_view.style.display = "none";
+    list_games_view.style.display = "none";
+    document.getElementById(view).style.display = "block";
   }
 
-//"var request = new XMLHttpRequest()     request.open('GET', ";
+  function createGame(){
+    request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            result = JSON.parse(request.response);
+            if (result["status"] == "success"){
+                viewGame(result["room"]);
+            } else {
+
+            }
+        }
+    }
+    request.open("GET", "/api/new?player_name=" + getCookie("player_name"), true);
+    request.send();
+  }
+
+  function viewGame(game_id){
+    request = new XMLHttpRequest();
+    toggleView("in-game-view");
+  }
