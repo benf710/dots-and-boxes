d05cdabb225edfc7fa8c7310139868c6221b6dc7
Ben Foltz <ben.h.foltz@gmail.com>
2020-06-27 20:01:21 -0400

    working list games

--- static/game-ui.js
+++ static/game-ui.js
@@ -48,4 +48,63 @@ function change_player(){
         console.log("Enter a playername");
     }
 }
+
+function getAllGames(){
+    error = false;
+    request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            games_obj = JSON.parse(request.response);
+            game_ids = Object.keys(games_obj);
+            console.log("Request completed");
+            if (game_ids.length == 0){
+                document.getElementById("list-games-view").innerHTML = '<p class="lead">No games available.</p>';
+            } else {
+                list_builder = "";            
+                game_ids.forEach (function(element) {
+                    console.log(element);
+                    valid_id_rex = /^[0-9]+$/;
+                    if (valid_id_rex.test(element)){
+                        console.log('id passed');
+                        valid_value_rex = /^[a-zA-Z0-9\. ]+$/;
+                        game_object = games_obj[element];
+                        status = game_object['status'];
+                        if (valid_value_rex.test(status)){
+                            console.log('status passed');
+                            list_builder += `<li><a href="/api/room/${element}/status">${element} - ${status}</a></li>`;
+                        } else {
+                            document.getElementById("list-games-view").innerHTML = '<p class="lead">Invalid response from server.</p>';
+                            error = true;
+                            console.log('error at valid_status');
+                        }
+                    } else {
+                        document.getElementById("list-games-view").innerHTML = '<p class="lead">Invalid response from server.</p>';
+                        error = true;
+                        console.log('error at valid_id');
+                    }
+                })
+                if (error == false){
+                    document.getElementById("games-list").innerHTML = list_builder;
+                }
+            }
+            toggleView("list-games-view");
+        }
+    }
+    request.open("GET", "/api/all", true);
+    request.send();
+}
+
+function toggleView(view) {
+    pn_diag = document.getElementById("playername-diag");
+    if (pn_diag.style.display === "none") {
+        index_view = document.getElementById("index-view");
+        list_games_view = document.getElementById("list-games-view");
+        in_game_view = document.getElementById("in-game-view");
+        index_view.style.display = "none";
+        in_game_view.style.display = "none";
+        list_games_view.style.display = "none";
+        document.getElementById(view).style.display = "block";
+    }
+  }
+
 //"var request = new XMLHttpRequest()     request.open('GET', ";
